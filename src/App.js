4bf675949a1e882146cacb00ab462c2bb7311a8d
Caciqxe <sebastian.cartes.s@gmail.com>
2022-12-26 19:32:20 -0300

    saludo daikiwi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Daikiwi, estamos arriba!!
         </p>
         <a
           className="App-link"
